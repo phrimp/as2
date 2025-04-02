8508675c8708bf99a8c8bc7f6213fce257695f88
Phrimp <thaiphienn@gmail.com>
2025-04-02 15:22:59 +0700

    add repo

--- PhienNTBlazor/Program.cs
+++ PhienNTBlazor/Program.cs
@@ -1,6 +1,7 @@
 ﻿using PhienNTBlazor.Components;
 using Microsoft.EntityFrameworkCore;
 using Repository;
+using Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDbContextFactory<NewsSystemContext>(options => {
+    var connectionString = builder.Configuration.GetConnectionString("NewsSystemConnection");
+    options.UseSqlServer(connectionString);
+});
+
 builder.Services.AddScoped<INewsArticleRepo, NewsArticleRepo>();
+builder.Services.AddScoped<ICategoryRepo, CategoryRepo>();
+builder.Services.AddScoped<ITagRepo, TagRepo>();
+builder.Services.AddScoped<IAccountRepo, AccountRepo>();
 
 var app = builder.Build();
 
